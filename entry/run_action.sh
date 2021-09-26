95b975ab6cb3b6418425f90a058770bd36ac2b95
Adam Ormsby <ormsbyadam@gmail.com>
2021-09-26 13:19:18 -0500

    Ad git config to 'run' branch

--- entry/run_action.sh
+++ entry/run_action.sh
@@ -1,1 +1,10 @@
 #!/bin/sh
+
+# shellcheck disable=SC1091
+# config git settings
+. "${ACTION_PARENT_DIR}"/run/config_git.sh
+config_for_action
+
+# git config cleanup for workflow continuation
+# function from config_git.sh
+reset_git_config
