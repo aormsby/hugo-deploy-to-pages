78ab11eafe16bd0f2bd597af413d74d35db6a7ca
Adam Ormsby <ormsbyadam@gmail.com>
2021-09-28 20:48:55 -0500

    Add touch on deploy data file, store name in var

--- run/io_build_data.sh
+++ run/io_build_data.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+DEPLOY_DATA_FILENAME="hugo-deploy.dat"
+
 KEY_LAST_HASH="LAST_HASH"
 KEY_LAST_BUILD_NUMBER="LAST_BUILD_NUMBER"
 
 # retrieve last build data from hugo-deploy.dat
 read_build_data() {
-    if [ -f "hugo-deploy.dat" ]; then
+    if [ -f "${DEPLOY_DATA_FILENAME}" ]; then
         LAST_HASH=$(grep "${KEY_LAST_HASH}=" dattest.dat | cut -d'=' -f2)
         LAST_BUILD_NUMBER=$(grep "${KEY_LAST_BUILD_NUMBER}=" dattest.dat | cut -d'=' -f2)
 
@@ -14,9 +16,10 @@ read_build_data() {
             [ -z "${LAST_BUILD_NUMBER}" ]; then
             LAST_HASH=0
             LAST_BUILD_NUMBER=0
-            write_out "y" "WARNING - 'hugo-deploy.dat' build data found. Resetting build data and forcing a build to create the missing data."
+            write_out "y" "WARNING - '${DEPLOY_DATA_FILENAME}' build data found. Resetting build data and forcing a build to create the missing data."
         fi
     else
+        touch "${DEPLOY_DATA_FILENAME}"
         # shellcheck disable=SC2034
         LAST_HASH=0
         # shellcheck disable=SC2034
@@ -26,16 +29,17 @@ read_build_data() {
 
 # update stored build data after successful build
 update_build_data() {
-    LAST_HASH="${CURRENT_SOURCE_HEAD}"
+    # LAST_HASH="${CURRENT_SOURCE_HEAD}"
+    LAST_HASH="${GITHUB_SHA}"
     LAST_BUILD_NUMBER=$((LAST_BUILD_NUMBER + 1))
 }
 
 write_build_data() {
-    # clear file data
-    echo >hugo-deploy.dat
+    # # clear file data
+    # echo >"${DEPLOY_DATA_FILENAME}"
 
     echo "${KEY_LAST_HASH}=${LAST_HASH}
-    ${KEY_LAST_BUILD_NUMBER}=${LAST_BUILD_NUMBER}" >>hugo-deploy.dat
+    ${KEY_LAST_BUILD_NUMBER}=${LAST_BUILD_NUMBER}" >"${DEPLOY_DATA_FILENAME}"
     COMMAND_STATUS=$?
 
     if [ "${COMMAND_STATUS}" != 0 ]; then
@@ -43,6 +47,6 @@ write_build_data() {
         write_out "${COMMAND_STATUS}" "Build data could not be written for some reason. Try again, and please file a detailed issue on Github if the problem persists."
     fi
 
-    write_out -1 "Build data written to 'hugo-deploy.dat'"
+    write_out -1 "Build data written to '${DEPLOY_DATA_FILENAME}'"
     write_out "g" "SUCCESS\n"
 }
